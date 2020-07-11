95521d78f6220d546fa025fd5421b24f7611e32d
bossjoker1 <1397157763@qq.com>
2020-07-11 17:46:10 +0800

    ui and logic viewModel

--- app/src/main/java/com/example/sunnyweather/SunnyWeatherApplication.kt
+++ app/src/main/java/com/example/sunnyweather/SunnyWeatherApplication.kt
@@ -6,6 +6,7 @@ import android.content.Context
 //全局化获得context
 class SunnyWeatherApplication :Application(){
     companion object{
+        const val TOKEN = "kZXXScEUPHru8O0e"
         @SuppressLint("StaticFieldLeak")
         lateinit var context: Context
     }
