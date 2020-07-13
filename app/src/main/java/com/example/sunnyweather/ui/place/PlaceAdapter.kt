0924e244a7f276395e0c1f47f903eb3bfc8a5b2b
bossjoker1 <1397157763@qq.com>
2020-07-13 09:51:55 +0800

    可切换城市，功能完成

--- app/src/main/java/com/example/sunnyweather/ui/place/PlaceAdapter.kt
+++ app/src/main/java/com/example/sunnyweather/ui/place/PlaceAdapter.kt
@@ -10,6 +10,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.sunnyweather.R
 import com.example.sunnyweather.logic.model.Place
 import com.example.sunnyweather.ui.weather.WeatherActivity
+import kotlinx.android.synthetic.main.activity_weather.*
 
 //配置place适配器(常规)
 class PlaceAdapter(private val fragment: PlaceFragment, private val placeList: List<Place>):
@@ -27,14 +28,23 @@ class PlaceAdapter(private val fragment: PlaceFragment, private val placeList: L
         holder.itemView.setOnClickListener {
             val position = holder.adapterPosition
             val place = placeList[position]
-            val intent  = Intent(parent.context, WeatherActivity::class.java).apply{
-                putExtra("location_lng", place.location.lng)
-                putExtra("location_lat", place.location.lat)
-                putExtra("place_name", place.name)
+            val activity = fragment.activity
+            if (activity is WeatherActivity){
+                activity.drawerLayout.closeDrawers()
+                activity.viewModel.locationLat = place.location.lat
+                activity.viewModel.locationLng = place.location.lng
+                activity.viewModel.placeName = place.name
+                activity.refreshWeather()
+            }else {
+                val intent = Intent(parent.context, WeatherActivity::class.java).apply {
+                    putExtra("location_lng", place.location.lng)
+                    putExtra("location_lat", place.location.lat)
+                    putExtra("place_name", place.name)
+                }
+                fragment.startActivity(intent)
+                activity?.finish()
             }
             fragment.viewModel.savePlace(place)//保存数据
-            fragment.startActivity(intent)
-            fragment.activity?.finish()
         }
         return holder
     }
